09bb29fcb32c7d1f2a1ba7e557ecc262cf09a7b2
Cerem Cem ASLAN <cem@aktos.io>
2018-04-23 22:40:49 +0300

    speedup

--- reset-on-ping-loss.sh
+++ reset-on-ping-loss.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/aktos-bash-lib/basic-functions.sh
 
 if [ "$(id -u)" != "0" ]; then 
         sudo $0 $@
@@ -25,14 +26,10 @@ while true; do
 	if ! is_network_reachable; then 
         echo "Network seems not reachable, waiting for 10 seconds for refreshing connection"
         published_reachable=
-        sleep 10s
-    	if ! is_network_reachable; then
-			echo "Network is still unreachable, reloading networking..."
-            $DIR/reset-interfaces.sh
-	   fi
+        $DIR/reset-interfaces.sh
 	else
-    	[ -z $published_reachable ] && echo "Network is reachable, doing nothing."
+    	[ -z $published_reachable ] && echo_green "Network is reachable, doing nothing."
         published_reachable="yes"
-	    sleep 2s
+	    sleep 5s
     fi
 done
