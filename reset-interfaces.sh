5f3d90fcfe9f426201d29e496171b66c997f3a86
Cerem Cem ASLAN <cem@aktos.io>
2018-04-23 22:40:36 +0300

    fixed reset-interfaces procedure

--- reset-interfaces.sh
+++ reset-interfaces.sh
@@ -1,8 +1,15 @@
 #!/bin/bash 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/aktos-bash-lib/basic-functions.sh
+
+echo "Removing lease files..."
 rm /var/lib/dhcp/dhclient* 2> /dev/null
 
 iface="eth0"
-echo "Requesting DHCP for $iface"
 #/etc/init.d/networking reload
-timeout 5s dhclient $iface 2> /dev/null
+dhclient -r $iface # killing old client
+ifconfig $iface down
+ifconfig $iface up
+echo "Requesting DHCP for $iface"
+timeout 5s dhclient -v $iface 2> /dev/null
 [[ "$?" == "0" ]] || echo "..failed getting dhcp response for $iface"
